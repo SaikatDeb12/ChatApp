e3f90f15adb413b6bd1402b08a899d926172f6ed
SaikatDeb12 <saikat.rcld@gmail.com>
2025-06-28 12:20:02 +0530

    disconnect logic, frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,18 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
+let connections: WebSocket[] = [];
 wss.on("connection", (socket) => {
   console.log("connected");
+  connections.push(socket);
   socket.on("message", (message) => {
-    console.log(message.toString());
-    setTimeout(() => {
-      socket.send(message.toString() + "send by server");
-    }, 2000);
+    //broadcast:
+    connections.forEach((s) => {
+      s.send(message.toString());
+    });
+  });
+
+  socket.on("close", (socket: WebSocket) => {
+    connections.filter((x: WebSocket) => x != socket);
   });
 });
---
+++ frontend/src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+function App() {
+  return <div>Chat app</div>;
+}
+
+export default App;
